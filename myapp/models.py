6b4c38f9f5a08dab6b19fa33754dd4989362544d
shinegit1 <sonampanwar471@gmail.com>
2023-02-26 18:57:00 +0530

    update it.

--- myapp/models.py
+++ myapp/models.py
@@ -6,3 +6,4 @@ class Contact:
     name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     address = models.CharField(max_length=40)
+    country = models.CharField(max_length=30)
