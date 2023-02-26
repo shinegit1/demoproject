82566b950517c24fe71f88ae083215f49b17c9f3
shinegit1 <sonampanwar471@gmail.com>
2023-02-26 20:09:05 +0530

    update it here.

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 
-# Create your models here.
-class Contact:
+class Contact(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     address = models.CharField(max_length=40)
